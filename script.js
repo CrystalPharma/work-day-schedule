4052c1691a6001d435f5da90abe8e39b78f1b2ba
CrystalPharma <crystalpharmauk@gmail.com>
2022-12-12 14:44:25 +0000

    current date moment.js

---
+++ script.js
@@ -0,0 +1,12 @@
+//Time display for current day and time
+var time = moment().format('dddd MMMM Do YYYY');
+$("#currentDay").text(time);
+
+// Timeblocks display for standard business hours create elements
+
+
+//Color-code for past, present and future
+
+// Enter event when select on timeblock
+
+// Save event local storage upon clicking save botton
