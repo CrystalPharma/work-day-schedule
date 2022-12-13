8ba34d47ec4188966312339fea3b1781b5aae99e
CrystalPharma <crystalpharmauk@gmail.com>
2022-12-13 11:25:50 +0000

    create paragraph

--- script.js
+++ script.js
@@ -6,47 +6,61 @@ $("#currentDay").text(time);
 var totalHours = 17 - 9; //8 hrs schedule from 9am until 17:00pm
 // console.log(totalHours);
 var currentTime = moment().format('HH');
-var timeTableElement = moment().startOf('day').add(9, 'hours').format('HH:mm A');;
+console.log(currentTime);
 var stageOfTime;
 var selectedTime;
 // // console.log (currentTime);
 // console.log(startTime);
+var timeContainer = document.querySelector('#time-contain');
+var timeContainerEl = document.createElement("p");
+timeContainer.appendChild(timeContainerEl);
+
+for (var hour = 9; hour <= 17; hour ++) {
+    timeContainerEl.innerHTML = hour ;
+}
+
+
+
 
 //Color-code for past, present and future
+// for (var i = 9; i <= 17; i++) {
+    
+//     if (selectedTIme == currentTime) {
+//         stageOfTime == 'present';
+//     }
+//     else if (selectedTIme > currentTime) {
+//         stageOfTime == 'future';
+//     }
+//     else {
+//         stageOfTime == 'past';
+//     }
 
-function createTimeSchedule() {
-    for (var i = 9; i <= 17; i++) {
-        var timeTableElement = moment().startOf('day').add(9, 'hours').format('HH:mm A');;
-        var currentTime = i;
-        if (selectedTIme == currentTime) {
-            stageOfTime == 'present';
-        }
-        else if (selectedTIme > currentTime) {
-            stageOfTime == 'future';
-        }
-        else {
-            stageOfTime == 'past';
-        }
-        var tableBlock =
-            `<div id="hour-${currentTime}" class="row time-block ${stageOfTime}">
-            <div class="col-md-1 hour">${timeTableElement}</div>
-            <textarea class="col-md-10 description ${currentTime}"></textarea>
-            <button class="btn saveBtn col-md-1">
-            <i class="fas fa-save"></i>
-            </button>
-            </div>`;
-        $('.container').append(tableBlock);
-    }
-    loadSchedule();
-}
+// }
 
-function loadSchedule() {
+// function createTimeSchedule() {
+//     for (var i = 9; i <= 17; i++) {
+    
+//         if (selectedTIme == currentTime) {
+//             stageOfTime == 'present';
+//         }
+//         else if (selectedTIme > currentTime) {
+//             stageOfTime == 'future';
+//         }
+//         else {
+//             stageOfTime == 'past';
+//         }
+   
+//     }
 
-    for (var i = 9; i <= 17; i++) {
-        var currentTime = i;
-        var loadedSchedule = localStorage.getItem(`hour-${currentTime}`);
+// }
 
-        $(`.${currentTime}`).val(loadedSchedule);
-    }
+// function loadSchedule() {
 
-}
+//     for (var i = 9; i <= 17; i++) {
+//         var currentTime = i;
+//         var loadedSchedule = localStorage.getItem(`hour-${currentTime}`);
+
+//         $(`.${currentTime}`).val(loadedSchedule);
+//     }
+
+// }
